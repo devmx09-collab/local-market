3e884af40d63c44ebcae0600bc0e984c6291d0a7
devmx09-collab <devmx09@gmail.com>
2026-07-12 11:25:20 -0600

    Add global styles and metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
 
+import "./globals.css";
+
+export const metadata = {
+  title: "Unit",
+  description: "Real local stuff. No nonsense.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
